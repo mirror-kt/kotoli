dbf5dc636294ea19f56f8b41fe1861841d933828
mirror-kt <me@mirror-kt.dev>
2020-10-25 23:22:09 +0900

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.4.10"
+}
+
+group = "dev.mirror-kt"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
